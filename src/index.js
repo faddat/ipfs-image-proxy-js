d45ba54bb81cb0f9a869291129f266cfbe222981
root <jacobgadikian@gmail.com>
2020-09-29 18:11:13 +0200

    First commit.  Credit for prior work goes to @baabeetaa

--- src/index.js
+++ src/index.js
@@ -6,13 +6,28 @@ let { PrivateKey, PublicKey, Signature } = require('@blurtfoundation/blurtjs/lib
 const RIPEMD160 = require('ripemd160');
 const AWS = require('aws-sdk');
 const {RateLimiterMemory} = require('rate-limiter-flexible');
+const ipfsClient = require('ipfs-http-client');
+const ipfsCluster = require('ipfs-cluster-api');
 
+
+
+// Set up blurtjs
 chainLib.api.setOptions({
-  url: process.env.JSONRPC_URL,
+  url: "http://localhost:8091",
   retry: true,
   useAppbaseApi: true,
 });
 
+
+//Connect to local ipfs node
+const ipfs = ipfsClient('http://localhost:5001');
+
+
+// connect to ipfs daemon API server
+const cluster = ipfsCluster(); 
+
+
+// Setup AWS
 const s3 = new AWS.S3({
   accessKeyId: process.env.S3_ACCESS_KEY,
   secretAccessKey: process.env.S3_SECRET_KEY,
@@ -21,9 +36,10 @@ const s3 = new AWS.S3({
   signatureVersion: "v4"
 });
 
+// Set up rate limiting
 const rate_limit_opts = {
   points: process.env.RATE_LIMIT_POINTS,          // 3 images
-  duration: 3600, // per hour
+  duration: 600, //  everry ten minutes
 };
 const rateLimiter = new RateLimiterMemory(rate_limit_opts);
 
@@ -77,8 +93,9 @@ hdl_upload_s3 = async (req, res) => {
       throw new Error("File size too big!");
     }
 
-    const hash_buffer = (new RIPEMD160().update(buffer).digest('hex'));
-    const s3_file_path = `${username}/${hash_buffer}.${file_ext}`;
+    // generate a hash // no longer writing to s3
+    //const hash_buffer = (new RIPEMD160().update(buffer).digest('hex'));
+    //const s3_file_path = `${username}/${hash_buffer}.${file_ext}`;
 
     { // verifying sig
       let isValidUsername = chainLib.utils.validateAccountName(username);
@@ -116,23 +133,37 @@ hdl_upload_s3 = async (req, res) => {
 
     await rateLimiter.consume(username, 1);
 
-    await s3.putObject({
-      ACL: 'public-read',
-      Bucket: process.env.S3_BUCKET,
-      Key: s3_file_path,
-      Body: buffer,
-      ContentType: content_type
-    }).promise();
+ //   await s3.putObject({
+ //     ACL: 'public-read',
+ //     Bucket: process.env.S3_BUCKET,
+ //     Key: s3_file_path,
+ //     Body: buffer,
+ //     ContentType: content_type
+ //   }).promise();
 
-    const img_full_path = `${process.env.PREFIX_URL}${process.env.S3_BUCKET}/${s3_file_path}`;
+
+    const results = ipfs.add(buffer);
+    for await (const { cid } of results) {
+  // CID (Content IDentifier) uniquely addresses the data
+  // and can be used to get it again.
+    console.log(cid.toString());
+
+}
+   cluster.pin.add(cid.toString(), (err) => {
+	err ? console.error(err) : console.log('pin added')
+})
+
     // this.body = JSON.stringify({status: 'ok', message: 'success', data: img_full_path});
-    res.json({status: 'ok', message: 'success', data: img_full_path});
+    res.json({status: 'ok', message: 'success', data: cid.toString()});
   } catch (e) {
     // console.error('Error in /imageupload api call', this.session.uid, error);
     res.json({status: 'error', message: e.message, data: e});
   }
 };
 
+
+
+// Configure the exprress server
 serverStart = () => {
   app.use(bodyParser.json({type: 'application/json', limit: '10mb'}));
 
@@ -148,6 +179,8 @@ serverStart = () => {
   console.log('serverStart on port ' + port);
 };
 
+
+// Start the express server
 serverStart();
 
 module.exports = app;
